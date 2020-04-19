545bb1b0cd6ab02ad538b8ad5fd7e0fe5808960e
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2020-04-19 18:18:34 +0500

    some changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,39 +2,46 @@ import { https } from 'firebase-functions';
 import { WebhookClient } from 'dialogflow-fulfillment';
 import { ServerClient } from "postmark";
 
-export const webhook = https.onRequest((request, response) => {
-    const _agent = new WebhookClient({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+export const webhook = https.onRequest(async (request, response) => {
+    try {
 
-    function getEmail(agent: WebhookClient) {
-        const serverToken = "92220d22-388d-4063-9053-924c4245bb83";
-        const client = new ServerClient(serverToken);
+        const _agent = new WebhookClient({ request, response });
+        console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+        console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-        client.sendEmail({
-            "From": "contact@supportgrow.com",
-            "To": "gist-edcclxgk@inbound.gistmail1.com",
-            "Subject": "email Support request",
-            "TextBody": `Hey,
+        function getEmail(agent: WebhookClient) {
+            const serverToken = "0cb9fc14-bdcb-4616-9be7-fa501b6c39a9";
+            const client = new ServerClient(serverToken);
+
+            client.sendEmail({
+                "From": "contact@supportgrow.com",
+                "To": "gist-edcclxgk@inbound.gistmail1.com",
+                "Subject": "email Support request",
+                "TextBody": `Hey,
             I was trying to talk with you chatbot on https://youngofficial.com/test-bot/ but I didn't get my problem solved
             I am leaving my email address for an agent to guide me: ${agent.parameters.email}
             
             thanks`
 
-        }).then((sendingResponse) => {
-            console.log("sending response: ", sendingResponse);
-            agent.add(`Awesome. your email noted as ${agent.parameters.email} We will be in touch soon.`);
+            }).then((sendingResponse) => {
+                console.log("sending response: ", sendingResponse);
+                agent.add(`Awesome. your email noted as ${agent.parameters.email} We will be in touch soon.`);
+                return;
 
-        }).catch(e => {
-            console.log("error: ", e);
-            agent.add("unable to send email postmark is under review");
-        });
+            }).catch(e => {
+                console.log("error: ", e);
+                agent.add("unable to send email postmark is under review");
+                return;
+            });
+        }
+
+        const intentMap = new Map();
+        intentMap.set('GetEmail', getEmail);
+
+        // tslint:disable-next-line: no-floating-promises
+        _agent.handleRequest(intentMap);
+    } catch (e) {
+        console.log("main error catch: ", e);
     }
-
-    const intentMap = new Map();
-    intentMap.set('GetEmail', getEmail);
-
-    // tslint:disable-next-line: no-floating-promises
-    _agent.handleRequest(intentMap);
 });
 
