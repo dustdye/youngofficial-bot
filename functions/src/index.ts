4b3825942f86b5349559218f3da693de0346031d
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2020-04-27 02:25:43 +0500

    minors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,15 +60,16 @@ export const webhook = https.onRequest(async (request, response) => {
             console.log("request.body: ", JSON.stringify(request.body));
             // console.log("agent.contexts.botcopy-ref-context: ", agent.contexts["botcopy-ref-context"]);
 
-            agent.add("I have received these in context: " + JSON.stringify(agent.contexts))
-            agent.add("everything I received in request is here: " + JSON.stringify(request.body))
+            // agent.add("I have received these in context: " + JSON.stringify(agent.contexts))
+            // agent.add("everything I received in request is here: " + JSON.stringify(request.body))
 
-
+            agent.add("Hi! Can I help you? 😁")
 
         }
+
         const intentMap = new Map();
         intentMap.set('GetEmail', getEmail);
-        intentMap.set('refTest', refTest);
+        intentMap.set('Default Welcome Intent', refTest);
 
         // tslint:disable-next-line: no-floating-promises
         _agent.handleRequest(intentMap);
@@ -78,3 +79,29 @@ export const webhook = https.onRequest(async (request, response) => {
 });
 
 
+
+
+
+const a = {
+    "responseId": "16c94138-135b-4241-83b7-7145de74ae25-eec93b43",
+    "queryResult": {
+        "queryText": "GOOGLE_ASSISTANT_WELCOME",
+        "action": "input.welcome",
+        "parameters": {},
+        "allRequiredParamsPresent": true,
+        "fulfillmentText": "Hi! Can I help you? 😁",
+        "fulfillmentMessages": [{ "text": { "text": ["Hi! Can I help you? 😁"] } }],
+        "outputContexts": [{
+            "name": "projects/ygy1-living/agent/sessions/887b5760-bd7b-0753-3173-46f0d417b979/contexts/visitorinteraction1",
+            "lifespanCount": 5
+        }],
+        "intent": {
+            "name": "projects/ygy1-living/agent/intents/185c0e7c-6f2c-461a-b723-af748d73712a",
+            "displayName": "Default Welcome Intent"
+        },
+        "intentDetectionConfidence": 1,
+        "languageCode": "en"
+    },
+    "originalDetectIntentRequest": { "payload": {} },
+    "session": "projects/ygy1-living/agent/sessions/887b5760-bd7b-0753-3173-46f0d417b979"
+}
